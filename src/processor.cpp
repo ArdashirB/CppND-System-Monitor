c41a1c22e4aa5eac1a9326c2fb7c0de3f64d36cb
Ardashir Bulsara <abulsar@clemson.edu>
2020-04-05 16:08:21 -0400

    Cpu utilization fixed

--- src/processor.cpp
+++ src/processor.cpp
@@ -3,19 +3,26 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <chrono>
+#include <thread>
+#include <unistd.h>
+
 using std::string;
 using std::stof;
 using std::stoi;
 using std::to_string;
 using std::vector;
 
+
 // TODO: Return the aggregate CPU utilization
 float Processor::Utilization() { 
-    float totaltime;
-    string line, key, value;
+    string line, key;
+    float totald, idled, total, prev_total;
+    float prev_idle, curr_idle;
+    float prev_non_idle, curr_non_idle;
+    float curr_total;
     string user, nice, system, idle, iowait, irq, softirq, steal, guest, guest_nice; 
-    float usertime, nicetime, systemalltime, idlealltime, iowaittime, irqtime, softirqtime,
-         stealtime, guesttime, guest_nicetime, virtalltime;
+
     std::ifstream filestream(LinuxParser::kProcDirectory + LinuxParser::kStatFilename);
     if (filestream.is_open()) {
         while (std::getline(filestream, line)) {
@@ -23,18 +30,35 @@ float Processor::Utilization() {
             while (linestream >> key >> user >> nice >> system >> idle >> iowait >>
                     irq >> softirq >> steal >> guest >> guest_nice) {
                 if (key == "cpu") {
-                    usertime = stoi(user) - stoi(guest);
-                    nicetime = stoi(nice) - stoi(guest_nice);
-                    idlealltime = stoi(idle) + stoi(iowait);
-                    systemalltime = stoi(system) + stoi(irq) + stoi(softirq);
-                    virtalltime = stoi(guest) + stoi(guest_nice);
-                    totaltime = usertime + nicetime + idlealltime + systemalltime +
-                                 stoi(steal) + virtalltime;
-                    return totaltime;
+                    prev_idle = stoi(idle) + stoi(iowait);
+                    prev_non_idle = stoi(user) + stoi(nice)+ stoi(system) + 
+                                    stoi(irq) + stoi(softirq) + stoi(steal);
+                    
                 }
             }
         }
     }
-//   return std::stoi(value);
-  return totaltime; 
+    usleep(100000);
+
+    std::ifstream filestream2(LinuxParser::kProcDirectory + LinuxParser::kStatFilename);
+    if (filestream2.is_open()) {
+        while (std::getline(filestream2, line)) {
+            std::istringstream linestream(line);
+            while (linestream >> key >> user >> nice >> system >> idle >> iowait >>
+                    irq >> softirq >> steal >> guest >> guest_nice) {
+                if (key == "cpu") {
+                    curr_idle = stoi(idle) + stoi(iowait);
+                    curr_non_idle = stoi(user) + stoi(nice)+ stoi(system) + 
+                                    stoi(irq) + stoi(softirq) + stoi(steal);
+                }
+            }
+        }
+    }
+
+    prev_total = prev_idle + prev_non_idle;
+    curr_total = curr_idle + curr_non_idle;
+    totald = curr_total - prev_total;
+    idled = curr_idle - prev_idle;
+
+    return (totald - idled) / totald ; 
 }
